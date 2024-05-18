e9ad61a9b34141e93e0d52e978c56b52c91f115b
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2024-05-19 03:20:11 +0800

    constructor

--- constructor/src/main/java/org/example/Main.java
+++ constructor/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Student student1 = new Student("Student-1", "1234", 3.5, 30);
+        Student student2 = new Student("Student-2", "5678", 3.6, 32);
     }
 }
--- constructor/src/main/java/org/example/Student.java
+++ constructor/src/main/java/org/example/Student.java
@@ -5,4 +5,16 @@ public class Student {
     String id;
     double gpa;
     int age;
+
+    public Student(String name, String id, double gpa, int age) {
+        this.name = name;
+        this.id = id;
+        this.gpa = gpa;
+        this.age = age;
+
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(gpa);
+        System.out.println(age);
+    }
 }
