e9d466364c721c446ced0ec5f0af7e1d3f22cfee
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2024-05-19 03:10:58 +0800

    Class ad Object

---
+++ classAndObject/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+//        student1.name("Student-1");
+//        student1.ID("1234");
+//        student1.GPA(3.5);
+//        student1.age(30);
+
+        Student student2 = new Student();
+    }
+}
---
+++ classAndObject/src/main/java/org/example/Student.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Student {
+    String name;
+    String ID;
+    double GPA;
+    int age;
+}
